78d535c9f24b292ffdc9c525ae93f45eda70ac3c
FourHyphen <FourHyphen.4@gmail.com>
2022-03-30 07:58:58 +0900

    ファイル読み込みトライ

--- LoadOtherScript/Tests/test_MainJobA.groovy
+++ LoadOtherScript/Tests/test_MainJobA.groovy
@@ -1,5 +1,6 @@
 def test_suite(def workspace_path, def unique_id) {
     // テスト用に pipeline ブロックを除いたスクリプトファイルを作成
+    // (pipeline ブロック残したままだと java.lang.IllegalStateException: Only one pipeline { ... } block can be executed in a single run.)
     def test_script_path = "${workspace_path}/${unique_id}_MainJobA.groovy"
     create_script_without_pipeline_block("${workspace_path}/LoadOtherScript/MainJobA.groovy", test_script_path)
 
@@ -11,6 +12,10 @@ def test_suite(def workspace_path, def unique_id) {
 
 def create_script_without_pipeline_block(def original_path, def create_path) {
     println("create_script_without_pipeline_block()")
+
+    def text = new File(original_path).text
+    println(text)
+
     println("create: ${create_path}")
 }
 
