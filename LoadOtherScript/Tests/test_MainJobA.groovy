dfe4f581eb30f1e466da8521d03d18e2c4067037
FourHyphen <FourHyphen.4@gmail.com>
2022-04-01 07:25:06 +0900

    改行対応

--- LoadOtherScript/Tests/test_MainJobA.groovy
+++ LoadOtherScript/Tests/test_MainJobA.groovy
@@ -40,7 +40,8 @@ def exclude_pipeline_block(String text) {
 def write_file(String file_path, String contents, String encoding="utf8") {
     // いちいち Scripts not permitted to use に対応するのが面倒なのでスクリプト処理
     // new File(file_path).setText(contents)
-    powershell(script: "Write-Output ${contents} | Set-Content -Encoding ${encoding} ${file_path}")
+    String here_document = @"${contents}"
+    powershell(script: "Write-Output ${here_document} | Set-Content -Encoding ${encoding} ${file_path}")
 }
 
 def load_script(String load_script_path) {
