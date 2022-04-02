3bc44a735af55586e6f0175896607d96f29ec4fc
FourHyphen <FourHyphen.4@gmail.com>
2022-04-02 17:11:17 +0900

    実行結果を記載

--- LoadOtherScript/Tests/test_MainJobA.groovy
+++ LoadOtherScript/Tests/test_MainJobA.groovy
@@ -7,7 +7,9 @@ def test_suite(String workspace_path, String unique_id) {
     // テスト用のスクリプトを load
     def script_MainJobA = load_script(test_script_path)
 
-    println("g_value1: ${script_MainJobA.g_value1}")
+    // java.lang.NullPointerException: Cannot get property 'g_value1' on null object
+    // println("g_value1: ${script_MainJobA.g_value1}")
+
     script_MainJobA.pre_process()
     println("g_value1: ${script_MainJobA.g_value1}")
 
