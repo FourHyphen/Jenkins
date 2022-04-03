6f2de6e1a667bd0b22132b5d82806eae6652b3d1
FourHyphen <FourHyphen.4@gmail.com>
2022-04-03 14:17:50 +0900

    コード修正

--- LoadOtherScript/Tests/test_MainJobA.groovy
+++ LoadOtherScript/Tests/test_MainJobA.groovy
@@ -53,7 +53,7 @@ def write_file(String file_path, String contents, String encoding="utf-8") {
 
 def create_ps_command_write_file(String file_path, String contents, String encoding) {
     def matching = /(?m)^return +this(\t| )*\r?\n/
-    boolean script_not_contains_return_this = !(text ==~ matching)
+    Boolean script_not_contains_return_this = !(text ==~ matching)
 
     return """
         \$splited = \"${contents}\".Replace("\r", "").Split("\n")
