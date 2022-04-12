8ac8ed786458e41d23b12fb5a13b8cabb8ee64c6
FourHyphen <FourHyphen.4@gmail.com>
2022-04-13 07:18:59 +0900

    ファイル出力をwriteFile()に変更

--- LoadOtherScript/Tests/common.groovy
+++ LoadOtherScript/Tests/common.groovy
@@ -4,20 +4,20 @@
 def load_script_edited_for_testing(String test_script_path, String workspace_path, String unique_id) {
     // テスト用に pipeline ブロックを除いたスクリプトファイルを作成
     String script_edited_for_testing_path = "${workspace_path}/${unique_id}_MainJobA.groovy"
-    create_script_edited_for_testing(test_script_path, script_edited_for_testing_path, workspace_path, unique_id)
+    create_script_edited_for_testing(test_script_path, script_edited_for_testing_path)
 
     // テスト用のスクリプトを load
     return load_script(script_edited_for_testing_path)
 }
 
-def create_script_edited_for_testing(String original_path, String create_path, String workspace_path, String unique_id) {
+def create_script_edited_for_testing(String original_path, String create_path) {
     // テスト用に pipeline ブロックなし＆ return this ありのスクリプト本文を作成し、保存する
     println("create_script_edited_for_testing()")
 
     String text = read_file(original_path)
     String contents = exclude_pipeline_block(text)
     contents = add_return_this(contents)
-    write_file(create_path, contents, workspace_path, unique_id)
+    write_file(create_path, contents)
 
     println("create: ${create_path}")
 }
@@ -62,62 +62,8 @@ def add_return_this(String contents) {
     return ret
 }
 
-def write_file(String file_path, String contents, String workspace_path, String unique_id) {
-    // いちいち Scripts not permitted to use に対応するのが面倒なのでスクリプト処理
-    // new File(file_path).setText(contents)
-    try {
-        write_file_linux(file_path, contents)
-    } catch (Exception e) {
-        write_file_windows(file_path, contents, workspace_path, unique_id)
-    }
-}
-
-def write_file_linux(String file_path, String contents) {
-    String sh_command = create_sh_command_write_file(file_path, contents)
-    sh(script: sh_command)
-}
-
-def create_sh_command_write_file(String file_path, String contents) {
-    String tmp_name = "tmp.groovy"
-    String sh_command = """
-    echo "{$contents}" > ${tmp_name}
-    iconv -t SJIS ${tmp_name} > ${file_path}
-    rm ${tmp_name}
-    """
-}
-
-def write_file_windows(String file_path, String contents, String workspace_path, String unique_id) {
-    // 文字列エスケープが複雑なため、検証可能なよう ps1 を出力する
-    String ps_path = "${workspace_path}\\${unique_id}_to_create_jenkinsfile.ps1"
-    String ps_command = create_ps_command_write_file(file_path, contents)
-    new File(ps_path).setText(ps_command)
-
-    powershell(script: "${ps_path}")
-}
-
-def create_ps_command_write_file(String file_path, String contents) {
-    String escaped = escape_script(contents)
-    return """
-        \$splited = \"${escaped}\".Replace("\r", "").Split("\n")
-
-        # StreamWriter 構築
-        \$enc = [System.Text.Encoding]::GetEncoding("SJIS")
-        \$sw = [System.IO.StreamWriter]::new(\"${file_path}\", \$false, \$enc)
-
-        # 1 行毎に記載することで powershell 用のエスケープをできるだけ回避
-        foreach (\$line in \$splited)
-        {
-            \$sw.WriteLine(\$line)
-        }
-
-        \$sw.Close()
-    """
-}
-
-def escape_script(String str) {
-    // ` と " のエスケープ -> Windows Powershell のエスケープ
-    // $ のエスケープ -> groovy のエスケープ(これやらないと ps の $splited 作成時に ${} が展開されてしまう)
-    return str.replace("`", "``").replace("\"", "`\"").replace("\$", "`\$")
+def write_file(String file_path, String contents) {
+    writeFile(file: file_path, text: contents)
 }
 
 def load_script(String load_script_path) {
